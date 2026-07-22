ee2fcf22b5c45ee6de19e5226b769b2892774fca
Yamen-Bk <bkyamen9@gmail.com>
2026-07-22 22:23:28 +0300

    تقارير المشاريع ux 2

--- client/src/pages/MonthlyReportPage.tsx
+++ client/src/pages/MonthlyReportPage.tsx
@@ -180,6 +180,7 @@ export default function MonthlyReportPage() {
           onSaveDraft={handleSaveDraft}
           onPrintPreview={() => setShowPreviewModal(true)}
           isSubmitting={isSubmitting}
+          onStatusChange={setReportStatus}
         />
 
         <div className="space-y-6">
@@ -490,18 +491,7 @@ export default function MonthlyReportPage() {
                 />
               </div>
 
-              <div className="space-y-1.5 pt-3 border-t border-border/60">
-                <Label className="text-xs font-bold">حالة التقرير</Label>
-                <Select value={reportStatus} onValueChange={setReportStatus}>
-                  <SelectTrigger className="h-10 border-border/80 w-full md:w-64">
-                    <SelectValue placeholder="اختر حالة التقرير" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="تم الاطلاع" className="text-xs">تم الاطلاع (Reviewed)</SelectItem>
-                    <SelectItem value="معتمد" className="text-xs">معتمد (Approved)</SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
+
             </CardContent>
           </Card>
         </div>
