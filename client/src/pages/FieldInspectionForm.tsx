2f589d4555140a945455a1a98a671d55c5fde573
Yamen-Bk <bkyamen9@gmail.com>
2026-08-05 01:22:04 +0300

    تقرير سند القبض v17

--- client/src/pages/FieldInspectionForm.tsx
+++ client/src/pages/FieldInspectionForm.tsx
@@ -625,7 +625,7 @@ export default function FieldInspectionForm() {
             <FileUpload
               onFilesSelected={setAttachments}
               maxFiles={10}
-              acceptedTypes={["image/jpeg", "image/png", "image/webp"]}
+              acceptedTypes={["image/jpeg", "image/png", "image/webp", "image/heic", "image/heif", ".heic", ".heif"]}
               maxSizeMB={10}
               maxTotalSizeMB={10}
               label="رفع الصور"
