70a403bb9e4c9bdf8db398e69325af8665529620
Yamen-Bk <bkyamen9@gmail.com>
2026-08-25 18:13:10 +0300

    /forms-customization/services part1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -128,6 +128,8 @@ import StaffManagement from "./pages/StaffManagement";
 import ProgramCustomization from "./pages/ProgramCustomization";
 import FormsCustomization from "./pages/FormsCustomization";
 import FormsCustomizationEvaluation from "./pages/FormsCustomizationEvaluation";
+import FormsCustomizationServices from "./pages/FormsCustomizationServices";
+import FormsCustomizationServiceDetail from "./pages/FormsCustomizationServiceDetail";
 import FormsCustomizationOption2 from "./pages/FormsCustomizationOption2";
 import SupportTickets from "./pages/SupportTickets";
 import AdminGuard from "./components/AdminGuard";
@@ -394,6 +396,8 @@ function Router() {
       <Route path="/program-customization">{() => <AdminRoute component={ProgramCustomization} />}</Route>
       <Route path="/forms-customization">{() => <AdminRoute component={FormsCustomization} />}</Route>
       <Route path="/forms-customization/evaluation">{() => <AdminRoute component={FormsCustomizationEvaluation} />}</Route>
+      <Route path="/forms-customization/services">{() => <AdminRoute component={FormsCustomizationServices} />}</Route>
+      <Route path="/forms-customization/services/:serviceId">{() => <AdminRoute component={FormsCustomizationServiceDetail} />}</Route>
       <Route path="/forms-customization/option-2">{() => <AdminRoute component={FormsCustomizationOption2} />}</Route>
       <Route path="/notifications/customization">{() => <AdminRoute component={NotificationCustomization} />}</Route>
       <Route path="/support">
