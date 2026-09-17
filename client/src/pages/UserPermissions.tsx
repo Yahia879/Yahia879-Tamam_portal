8431565ca481478c030dbd689923bd9b3cfdabfe
Yamen-Bk <bkyamen9@gmail.com>
2026-09-17 11:23:27 +0300

    صلاحيات رضا المستفيدين 40

--- client/src/pages/UserPermissions.tsx
+++ client/src/pages/UserPermissions.tsx
@@ -636,7 +636,7 @@ export default function UserPermissions() {
         view: "عرض قسم التصعيد الإداري",
       },
       beneficiary_evaluations: {
-        view: "عرض قسم رضا المستفيدين",
+        view: "عرض إحصائيات رضا المستفيدين",
         evaluations_log: "سجل استبيانات التقييم",
         dispatch_log: "سجل الإرسال",
         contacts: "المستفيدين المعتمدين والاستفسارات",
