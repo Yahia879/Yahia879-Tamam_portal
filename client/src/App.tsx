39c9d2905c9f9ec5e9bdbcf782cdf1090999e80b
Yamen-Bk <bkyamen9@gmail.com>
2026-09-19 02:53:20 +0300

    تعاقد سدانة part 1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -35,6 +35,7 @@ import EditImam from "./pages/EditImam";
 import Requests from "./pages/Requests";
 import RequestDetails from "./pages/RequestDetailsNew";
 import SedanaEvaluationPage from "./pages/SedanaEvaluationPage";
+import SedanaProcurementPage from "./pages/SedanaProcurementPage";
 import RequestForm from "./pages/RequestForm";
 import TrackRequest from "./pages/TrackRequest";
 import MosqueServiceRequest from "./pages/MosqueServiceRequest";
@@ -287,6 +288,8 @@ function Router() {
       <Route path="/requests/:id" component={RequestDetails} />
       <Route path="/requests/:id/sedana-evaluation" component={SedanaEvaluationPage} />
       <Route path="/requester/requests/:id/sedana-evaluation" component={SedanaEvaluationPage} />
+      <Route path="/requests/:id/procurement" component={SedanaProcurementPage} />
+      <Route path="/requester/requests/:id/procurement" component={SedanaProcurementPage} />
       <Route path="/requests/:requestId/field-inspection">{() => <AdminRoute component={FieldInspectionForm} />}</Route>
       <Route path="/requests/:requestId/quick-response">{() => <AdminRoute component={QuickResponseReportForm} />}</Route>
       <Route path="/requests/:requestId/assign-final-report">{() => <AdminRoute component={AssignFinalReport} />}</Route>
